cca27ac3eb06c2b7469388875b931611ad50f465
Nico BUSUIOC <nicolaie.busuioc@gmail.com>
2023-06-15 20:20:56 +0200

    HW7: remove Methods

--- Mini Social/domain/Post.py
+++ Mini Social/domain/Post.py
@@ -7,6 +7,20 @@ class Post:
 
         # HW2 - direct relation
         self.author = author
+        self.comments = []
 
     def __str__(self):
         return f"Post({self.id})\n{self.title}\n{self.body}\n{self.author}\n"
+
+    def removeComment(self, comment):
+        for item in self.comments:
+            if item is comment:
+                self.comments.remove(item)
+                print(
+                    f"Comment with id: {comment.id} successfully deleted from the post list!")
+                break
+
+            # last element?
+            if item is self.posts[-1]:
+                raise KeyError(
+                    f"The comment with comment_id: {comment.id} was not found under the post with post_id: {self.id}")
--- Mini Social/domain/User.py
+++ Mini Social/domain/User.py
@@ -3,7 +3,6 @@ from .Comment import Comment
 
 
 class User:
-
     def __init__(self, id, username, email, password):
         self.id = id
         self.username = username
@@ -27,4 +26,33 @@ class User:
     def addComment(self, comm_id, comm_body, post):
         comment = Comment(comm_id, comm_body, self, post)
         self.comments.append(comment)
+        post.comments.append(comment)
         return comment
+
+    # HW 7
+    def removePost(self, post):
+        for item in self.posts:
+            if item is post:
+                self.posts.remove(item)
+                print(f"Post with id: {post.id} successfully deleted!")
+                break
+
+            # last element?
+            if item is self.posts[-1]:
+                raise KeyError(
+                    f"User {self.username} has no post with post_id: {post.id}")
+
+    def removeComment(self, comment):
+        for item in self.comments:
+            if item is comment:
+                self.comments.remove(item)
+                print(
+                    f"Comment with id: {comment.id} successfully deleted from user list")
+                # remove comment from the post list
+                item.post.removeComment(item)
+                break
+
+            # last element?
+            if item is self.posts[-1]:
+                raise KeyError(
+                    f"User {self.username} has no comment with comment_id: {comment.id}")
