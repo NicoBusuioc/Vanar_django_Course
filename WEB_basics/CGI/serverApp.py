fbb168d342985d5a2e5a255e6a8339841f7fc77e
Nico BUSUIOC <nicolaie.busuioc@gmail.com>
2023-08-31 22:36:04 +0200

    CGI part1 (not ready)

---
+++ WEB_basics/CGI/serverApp.py
@@ -0,0 +1,18 @@
+# webserver engines
+# builtin http server (pure Python solution)
+
+# from http.server import HTTPServer, SimpleHTTPRequestHandler
+
+# webServer = HTTPServer(("localhost", 8000), SimpleHTTPRequestHandler)
+# print("starting server...")
+# webServer.serve_forever()
+
+
+from http.server import HTTPServer, CGIHTTPRequestHandler
+import cgitb
+cgitb.enable()
+CGIHTTPRequestHandler.cgi_directories = ['/']
+
+webServer = HTTPServer(("localhost", 8000), CGIHTTPRequestHandler)
+print("starting server...")
+webServer.serve_forever()
